16ad6ff5a90af2eecde690d479b49a24d5fb57f5
FabioVisentainer <fabio_visentainer@hotmail.com>
2025-06-18 23:17:02 -0300

    feat: criar entidade PropertyModel para tabela tb_property
    Define a entidade JPA PropertyModel com os campos:
    
    - id como chave primária auto gerada
    - nome com validação de não vazio e tamanho entre 3 e 100 caracteres
    
    Mapeia a tabela "tb_property" no banco de dados.

---
+++ src/main/java/com/teste/cadastro/propriedades/PropertyModel.java
@@ -0,0 +1,37 @@
+package com.teste.cadastro.propriedades;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(
+        name = "tb_property"
+)
+public class PropertyModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "Nome não pode estar vazio!")
+    @Size(min = 3, max = 100 , message = "Nome deve ter entre 3 e 100 caracteres.")
+    private String nome;
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int propertyId) {
+        this.id = propertyId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String propertyName) {
+        this.nome = propertyName;
+    }
+}
