e4256a61cdb08ed07db3a0fe46727e944d2d8428
FabioVisentainer <fabio_visentainer@hotmail.com>
2025-06-19 21:39:55 -0300

    refactor: Renomear e refatorar a classe PropertyModel para Propriedade
    - Renomeada a classe `PropertyModel` para `Propriedade` para alinhar com a convenção de nomenclatura em português.
    - Modificada a anotação `@Table` para refletir a tabela `tb_propriedades` ao invés de `tb_property`.
    - Adicionado construtor adicional para facilitar a criação de instâncias da classe com o nome da propriedade.
    - Mantidas as validações de `@NotBlank` e `@Size` no campo `nome` para garantir que o nome seja válido.

--- src/main/java/com/teste/cadastro/propriedades/Propriedade.java
+++ src/main/java/com/teste/cadastro/propriedades/Propriedade.java
@@ -6,9 +6,9 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(
-        name = "tb_property"
+        name = "tb_propriedades"
 )
-public class PropertyModel {
+public class Propriedade {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,12 +18,15 @@ public class PropertyModel {
     @Size(min = 3, max = 100 , message = "Nome deve ter entre 3 e 100 caracteres.")
     private String nome;
 
+    public Propriedade() {}
+    public Propriedade(String nome) {
+        this.nome = nome;
+    }
+
     // Getters e Setters
     public int getId() {return id;}
-
-    public void setId(int propertyId) {this.id = propertyId;}
+    public void setId(int id) {this.id = id;}
 
     public String getNome() {return nome;}
-
-    public void setNome(String propertyName) {this.nome = propertyName;}
+    public void setNome(String nome) {this.nome = nome;}
 }
