51e3bdf0b17a83a095e30a67385154781d457652
FabioVisentainer <fabio_visentainer@hotmail.com>
2025-06-19 21:26:04 -0300

    Refactor: Renomear a classe 'LaboratoryModel' para 'Laboratorio' e alterar o nome da tabela para 'tb_laboratorios'
    - A classe foi renomeada para 'Laboratorio' para manter consistência com a língua portuguesa.
    - O nome da tabela foi alterado de 'tb_laboratory' para 'tb_laboratorios'.
    - Foi adicionado um construtor sem parâmetros e outro com o parâmetro 'nome'.

--- src/main/java/com/teste/cadastro/laboratorios/Laboratorio.java
+++ src/main/java/com/teste/cadastro/laboratorios/Laboratorio.java
@@ -6,9 +6,9 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(
-        name = "tb_laboratory"
+        name = "tb_laboratorios"
 )
-public class LaboratoryModel {
+public class Laboratorio {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,12 +18,15 @@ public class LaboratoryModel {
     @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres.")
     private String nome;
 
+    public Laboratorio() {}
+    public Laboratorio(String nome) {
+        this.nome = nome;
+    }
+
     // Getters e Setters
     public String getNome() {return nome;}
-
     public void setNome(String nome) {this.nome = nome;}
 
     public int getId() {return id;}
-
     public void setId(int id) {this.id = id;}
 }
